29e68257bc23f10d69ca8cf23d41999d6bbe1198
Yasmin Rocha <yasminnasrocha@gmail.com>
2025-12-04 18:33:48 -0300

    fix(front): Arrumando CSS do Form LoginPage e RegisterPage

--- todo-list/client/aps_ihc-todolist/src/pages/RegisterPage/registerFields.jsx
+++ todo-list/client/aps_ihc-todolist/src/pages/RegisterPage/registerFields.jsx
@@ -1,19 +1,19 @@
 export const registerFields = [
     {
         name: "name",
-        label: "Digite o seu nome",
+        label: "Digite o seu nome:",
         type: "text",
         placeholder: "Nome", 
     },
     {
         name: "email", 
-        label: "Digite o seu endereço de e-mail", 
+        label: "Digite o seu endereço de e-mail:", 
         type: "email", 
         placeholder: "E-mail", 
     },
     {
         name: "password", 
-        label: "Digite a sua senha", 
+        label: "Digite a sua senha:", 
         type: "password", 
         placeholder: "Senha", 
     }
